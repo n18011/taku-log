2ba0c10e2e4b171437778cab3d5e9e338060058d
n18011 <n18011@std.it-college.ac.jp>
2020-02-23 15:28:01 +0900

    add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,18 @@ module.exports = {
   plugins: [
     `gatsby-theme-material-ui`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'TT Log',
+        short_name: 'TTLog',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'standalone',
+        icon: 'src/images/logo.png', // This path is relative to the root of the site.
+      },
+    },
     `gatsby-plugin-offline`
 ],
 }
